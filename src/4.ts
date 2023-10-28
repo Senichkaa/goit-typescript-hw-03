00ee2c74fcd898d2f24547de29ca8bf5b4f79848
Senichkaa <super-kapanaiko406@ukr.net>
2023-10-28 09:38:14 +0300

    fixes after mentor's review

--- src/4.ts
+++ src/4.ts
@@ -38,7 +38,7 @@ abstract class House {
 }
 class MyHouse extends House {
   openDoor(key: Key): void {
-    if (key === this.key) {
+    if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
     }
   }
